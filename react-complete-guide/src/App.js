25941c2bbf8f8e3d93a75e3642988008cc6249c1
FelipeWoo <felipe.becerra.woo@gmail.com>
2021-04-09 00:24:10 -0500

    SECTION 3: Use of Classes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,13 +10,41 @@ import Person from './Person/Person';
 // JSX toma el código de los componentes y transforma en HTML
 //Lo ideal es invocar los componentes aquí y programarlos en su propio archivo
 class App extends Component {
+  //state es información interna
+  state = {
+    //lista de personas
+    persons: [
+      {name:'Maxi', age:28},
+      {name:'Manu', age:29},
+      {name:'Pedro', age:26}
+  ],
+  otherState: 'something'
+  }
+  //Arrow functon para lo que se va a hacer con el evento del botón
+  switchNameHandler = () => {
+    //console.log('El botón funciona');
+    // WRONG this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {name:'Maximilian', age:28},
+        {name:'Manu', age:29},
+        {name:'Pedro', age:27}
+    ]
+    })
+  }
+
+
   render() {
     return (
       //Agregar los componentes de la aplicación
       <div className="App">
         <h1>Hola, esta es la nueva App</h1>
         <p>Esto esta funcionando</p>
-        <Person />
+        <button onClick= {this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobies: Me gusta nadar los sabados por las mañanas</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
       </div>
     );
 
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,14 @@
 //Component
 
 import React from 'react';
-
-const person = () => {
-    return <p>Soy una persona</p>
+//props es para poder utilizar las propiedades del componente
+const person = (props) => {
+    return (
+        <div>
+            <p>Soy {props.name} y tengo {props.age} años.</p>
+            <p>{props.children}</p>
+        </div>
+            );
 };
 
 export default person;
