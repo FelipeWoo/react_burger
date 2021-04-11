b73fd86a71b0b610abfd2037467b70790151273a
FelipeWoo <felipe.becerra.woo@gmail.com>
2021-04-11 00:27:43 -0500

    SECTION 3: Assigment. Final Corrections

--- react-assigment-1/src/App.js
+++ react-assigment-1/src/App.js
@@ -11,19 +11,19 @@ const App = props => {
 
   //Dynamic use of username
   const [userNameState, setUserName] = useState({
-    usernames:[{username:'max69'}]
+    username:'max69'
   });
 
   //Click event handler of username
   const switchUserHandler = () => {
     setUserName({
-      usernames: [{username:'max699'}]  
+      username:'max699'  
     });
   }
   //Input event 
   const userChangeHandler = (event) => {
     setUserName({
-      usernames: [{username:event.target.value}]
+      username:event.target.value
     });
   }
 
@@ -32,9 +32,11 @@ const App = props => {
       <div className="App">
         <h1>First Assigment</h1>
         
-        <UserOutput username={userNameState.usernames[0].username}
+        <UserOutput username={userNameState.username}
         click={switchUserHandler}/>
-        <UserInput changed={userChangeHandler}/>
+        <UserInput 
+        changed={userChangeHandler}
+        currentName={userNameState.username}/>
       </div>
     );
   }
